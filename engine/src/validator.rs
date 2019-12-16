56c852bc97b6a65e8482568f5f3a8fe8479b01ea
Nathan Spaun <nspaun@fb.com>
2019-12-16 10:27:16 -0800

    Add function for validating moves for pieces

--- engine/src/validator.rs
+++ engine/src/validator.rs
@@ -1,8 +1,26 @@
 use crate::chess_move::ChessMove;
+use model::classification::Classification;
 
 use model::board::Board;
 
 pub fn validate_chess_move(chess_move: &ChessMove, board: &Board) -> Result<(), ChessMoveError> {
+    validate_move_is_on_board(chess_move, board)?;
+    validate_move_is_legal_for_piece(chess_move)?;
+    return Ok(());
+}
+
+fn validate_move_is_legal_for_piece(chess_move: &ChessMove) -> Result<(), ChessMoveError> {
+    return match chess_move.piece {
+        Classification::PAWN => Ok(()),
+        Classification::ROOK => Ok(()),
+        Classification::KNIGHT => Ok(()),
+        Classification::BISHOP => Ok(()),
+        Classification::KING => Ok(()),
+        Classification::QUEEN => Ok(()),
+    };
+}
+
+fn validate_move_is_on_board(chess_move: &ChessMove, board: &Board) -> Result<(), ChessMoveError> {
     let destination = chess_move.destination;
 
     if destination.0 > board.size.0 - 1 || destination.1 > board.size.1 - 1 {
